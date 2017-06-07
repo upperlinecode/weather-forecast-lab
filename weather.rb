702386ee907033209f83a73aa778f4d1e1babfd4
twant <taylor.want@gmail.com>
2017-06-07 08:20:27 -0500

    Update weather.rb

--- weather.rb
+++ weather.rb
@@ -7,7 +7,7 @@
 # }
 
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
-def location(weather)
+def location(weather_hash)
 	
 end
 
