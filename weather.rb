37a4517533d106e14548ecab86041ba0b1d5b2b1
twant <taylor.want@gmail.com>
2017-08-09 17:06:11 -0400

    Update weather.rb

--- weather.rb
+++ weather.rb
@@ -17,7 +17,7 @@ end
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
 
 
-#4. Write a method that takes in the weather hash as an argument. 
+#4. Write a method called "city_weather_info" that takes in the weather hash as an argument. 
 # You'll want to use the each method to iterate over the hash and print out
 # `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
 
